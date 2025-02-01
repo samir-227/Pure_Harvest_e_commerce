83a326201a6dc54eb4337c74169fd991af3192d2
samir-227 <samermedhat227@gmail.com>
2025-02-01 20:59:03 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitsHub());
+}
+
+class FruitsHub extends StatelessWidget {
+  const FruitsHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
