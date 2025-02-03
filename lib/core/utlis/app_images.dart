f9d2ce6fee03c839c7b2c07337def30e2127747e
samir-227 <samermedhat227@gmail.com>
2025-02-03 14:29:35 +0200

    splash view ui done

---
+++ lib/core/utlis/app_images.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+  
+  static const String assetsImagesPlant = "lib/assets/images/Plant.svg";
+
+  static const String assetsImagesSplashCircles = "lib/assets/images/splash-Circles.svg";
+
+  static const String assetsImagesSplashImage = "lib/assets/images/splash_image.svg";
+}
+
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruits_hub/core/utlis/app_images.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(AppImages.assetsImagesPlant),
+          ],
+        ),
+        SvgPicture.asset(AppImages.assetsImagesSplashImage),
+        SvgPicture.asset(
+          AppImages.assetsImagesSplashCircles,
+          fit: BoxFit.fill,
+        ),
+      ],
+    );
   }
 }
