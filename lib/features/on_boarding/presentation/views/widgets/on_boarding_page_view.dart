ca78473af55537d3e26824cacccde8da95165a06
samir-227 <samermedhat227@gmail.com>
2025-02-05 18:43:27 +0200

    OnBoardingPageView title done

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/models/page_view_item_model.dart';
+import 'package:fruits_hub/core/utlis/app_colors.dart';
 import 'package:fruits_hub/core/utlis/app_images.dart';
 import 'package:fruits_hub/core/utlis/app_text_style.dart';
 
@@ -11,27 +12,38 @@ class OnBoardingPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
-      children: const [
+      children: [
         PageViewItem(
           pageViewItemModel: PageViewItemModel(
             backgroundImage: AppImages.imagesItem1BackgroundImage,
             image: AppImages.imagesItem1Image,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Fruits',
+            title: Text.rich(
+              TextSpan(
+                text: ' Fruit',
+                style: TextStyles.bold23.copyWith(
+                  color: AppColors.primaryColor,
                 ),
-                Text(
-                  'Hub',
-                ),
-              ],
+                children: const [
+                  TextSpan(
+                    text: 'HUB',
+                    style: TextStyle(
+                      color: Color(0xFFF4A91F),
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' مرحبًا بك في',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
             ),
             subtitle:
                 'اكتشف تجربة تسوق فريدة مع FruitHUB. استكشف مجموعتنا الواسعة من الفواكه الطازجة الممتازة واحصل على أفضل العروض والجودة العالية.',
           ),
         ),
-        PageViewItem(
+        const PageViewItem(
           pageViewItemModel: PageViewItemModel(
             backgroundImage: AppImages.imagesItem2BackgroundImage,
             image: AppImages.imagesItem2Image,
@@ -41,7 +53,7 @@ class OnBoardingPageView extends StatelessWidget {
                 Text(
                   'ابحث وتسوق',
                   textAlign: TextAlign.center,
-                  style: TextStyles.semiBold13,
+                  style: TextStyles.bold23,
                 ),
               ],
             ),
